d7c5360c21ba340110d6dad55491b242ea9627cd
Yousuf Hasan <yousuf85@gmail.com>
2020-10-08 22:32:09 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createReadStream } from "fs";
 import { split } from "event-stream";
 import { JSONBuilder } from "./builder";
 
-const CSV_FILE_NAME = "node-data-processing-medium-data.csv";
+const CSV_FILE_NAME = "node-data-processing-medium-data.csv"; //hard coded file name just for demo
 
 export const readCSV = (builder: JSONBuilder<any>) => {
   createReadStream(CSV_FILE_NAME)
